59f60bb6de4fb5029e84505b4b3e1697950ba314
v3rena <v.poetzl@gmx.at>
2017-10-20 09:28:16 +0200

    Integration Tests work

--- code/PLS.SKS.Package.Services.Tests/IntegrationTests.cs
+++ code/PLS.SKS.Package.Services.Tests/IntegrationTests.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.AspNetCore.Hosting;
 using IO.Swagger.Models;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace PLS.SKS.Package.Services.Tests
 {
@@ -39,8 +42,12 @@ namespace PLS.SKS.Package.Services.Tests
 		[TestMethod]
 		public async Task PostWarehouse()
 		{
+			//Assert
+			var warehouse = new Warehouse("EX1234", "Test", 12, new List<Warehouse> { new Warehouse("EX1234", "Test", 12, new List<Warehouse>(), new List<Truck>()) }, new List<Truck>());
+			var stringContent = new StringContent(JsonConvert.SerializeObject(warehouse), Encoding.UTF8, "application/json");
+			
 			// Act
-			var response = await _client.GetAsync("/api/warehouse");
+			var response = await _client.PostAsync("/api/warehouse", stringContent);
 			response.EnsureSuccessStatusCode();
 
 			var responseString = await response.Content.ReadAsStringAsync();
@@ -53,8 +60,12 @@ namespace PLS.SKS.Package.Services.Tests
 		[TestMethod]
 		public async Task PostParcel()
 		{
+			//Assert
+			var parcel = new Parcel(12, new Receipient("Tobias", "Test", "Testgasse 7", "1160", "Wien"));
+			var stringContent = new StringContent(JsonConvert.SerializeObject(parcel), Encoding.UTF8, "application/json");
+
 			// Act
-			var response = await _client.GetAsync("/api/parcel");
+			var response = await _client.PostAsync("/api/parcel", stringContent);
 			response.EnsureSuccessStatusCode();
 
 			var responseString = await response.Content.ReadAsStringAsync();
@@ -67,12 +78,8 @@ namespace PLS.SKS.Package.Services.Tests
 		[TestMethod]
 		public async Task GetParcel()
 		{
-			//new Parcel() bauen und so weiter
-			var parcel = new Parcel(12, new Receipient("Tobias", "Test", "Testgasse 7", "1160", "Wien"));
-			//HttpContent body = null;
-
 			// Act
-			var response = await _client.GetAsync("/api/parcel/");
+			var response = await _client.GetAsync("/api/parcel/{trackingId}");
 			response.EnsureSuccessStatusCode();
 
 			var responseString = await response.Content.ReadAsStringAsync();
@@ -85,8 +92,14 @@ namespace PLS.SKS.Package.Services.Tests
 		[TestMethod]
 		public async Task PostReportHop()
 		{
+			//Assert
+			var parcel = new Parcel(12, new Receipient("Tobias", "Test", "Testgasse 7", "1160", "Wien"));
+			var warehouse = new Warehouse("EX1234", "Test", 12, new List<Warehouse> { new Warehouse("EX1234", "Test", 12, new List<Warehouse>(), new List<Truck>()) }, new List<Truck>());
+			var stringContent = new StringContent(JsonConvert.SerializeObject(parcel), Encoding.UTF8, "application/json");
+
+
 			// Act
-			var response = await _client.GetAsync("/api/parcel/{trackingId}/reportHop/{code}");
+			var response = await _client.PostAsync("/api/parcel/{trackingId}/reportHop/{code}", stringContent);
 			response.EnsureSuccessStatusCode();
 
 			var responseString = await response.Content.ReadAsStringAsync();
