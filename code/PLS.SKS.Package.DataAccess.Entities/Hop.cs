31fed78fc7900667cadd1a4fc196e98ea7fded06
v3rena <if15b028@technikum-wien.at>
2017-10-30 17:30:18 +0100

    Id property removed from truck and warehouse

--- code/PLS.SKS.Package.DataAccess.Entities/Hop.cs
+++ code/PLS.SKS.Package.DataAccess.Entities/Hop.cs
@@ -6,7 +6,7 @@ namespace PLS.SKS.Package.DataAccess.Entities
 {
     public class Hop
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string code { get; set; }
         public decimal duration { get; set; }
     }
