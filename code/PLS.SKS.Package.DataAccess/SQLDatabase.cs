61e5721a2498b6e4a1717871750032741fd8ada3
MrQorm <tobias.nemecek@gmx.at>
2017-11-13 10:21:10 +0100

    .

---
+++ code/PLS.SKS.Package.DataAccess/SQLDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PLS.SKS.Package.DataAccess.Interfaces;
+
+namespace PLS.SKS.Package.DataAccess
+{
+    public class SQLDatabase : Interfaces.IDatabase
+    {
+        public SQLDatabase()
+        {
+            hopArrivalRepo = new Sql.SqlHopArrivalRepository();
+            parcelRepo = new Sql.SqlParcelRepository();
+            recipientRepo = new Sql.SqlRecipientRepository();
+            trackingRepo = new Sql.SqlTrackingInformationRepository();
+            truckRepo = new Sql.SqlTruckRepository();
+            warehouseRepo = new Sql.SqlWarehouseRepository();
+        }
+
+        public IHopArrivalRepository hopArrivalRepo { get; }
+
+        public IParcelRepository parcelRepo { get; }
+
+        public IRecipientRepository recipientRepo { get; }
+
+        public ITrackingInformationRepository trackingRepo { get; }
+
+        public ITruckRepository truckRepo { get; }
+
+        public IWarehouseRepository warehouseRepo { get; }
+
+
+    }
+}
