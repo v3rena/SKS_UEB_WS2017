b017f351eadcc0870acbf0df6b51dd56565ee047
MrQorm <tobias.nemecek@gmx.at>
2017-11-13 13:57:12 +0100

    mapping issues fixed

--- code/PLS.SKS.Package.BusinessLogic/BusinessLogic.cs
+++ code/PLS.SKS.Package.BusinessLogic/BusinessLogic.cs
@@ -61,7 +61,9 @@ namespace PLS.SKS.Package.BusinessLogic
             Entities.Parcel blParcel = new Entities.Parcel(2.0f, blRec);
 
             IO.Swagger.Models.Parcel swParcel = Mapper.Map<IO.Swagger.Models.Parcel>(blParcel);
-
+            DataAccess.Entities.Parcel dalParcel = Mapper.Map<DataAccess.Entities.Parcel>(blParcel);
+            Entities.Parcel blParcel2 = Mapper.Map<Entities.Parcel>(dalParcel);
+            Entities.Parcel blParcel3 = Mapper.Map<Entities.Parcel>(swParcel);
             return;
         }
 
@@ -69,23 +71,46 @@ namespace PLS.SKS.Package.BusinessLogic
         {
             var config = new AutoMapper.MapperConfiguration(cfg =>
             {
+                //Swagger --> BL
                 cfg.CreateMap<IO.Swagger.Models.Recipient, Entities.Recipient>();
-                cfg.CreateMap<IO.Swagger.Models.Parcel, Entities.Parcel>().ForMember(model => model.TrackingInformation, option => option.Ignore()).ForMember(model => model.TrackingNumber, option => option.Ignore());
+                cfg.CreateMap<IO.Swagger.Models.Parcel, Entities.Parcel>()
+                    .ForMember(model => model.TrackingInformation, option => option.Ignore())
+                    .ForMember(model => model.TrackingNumber, option => option.Ignore());
                 cfg.CreateMap<IO.Swagger.Models.Warehouse, Entities.Warehouse>();
 				cfg.CreateMap<IO.Swagger.Models.Truck, Entities.Truck>(); //.ForMember(model => model.test, option => option.Ignore());
 				cfg.CreateMap<IO.Swagger.Models.TrackingInformation, Entities.TrackingInformation>();
-                cfg.CreateMap<IO.Swagger.Models.HopArrival, Entities.HopArrival>().ForMember(model => model.trackingInformation, option => option.Ignore());
-
+                cfg.CreateMap<IO.Swagger.Models.HopArrival, Entities.HopArrival>()
+                    .ForMember(model => model.trackingInformation, option => option.Ignore());
+                //BL --> Swagger
+                cfg.CreateMap<Entities.Parcel, IO.Swagger.Models.Parcel>();
+                cfg.CreateMap<Entities.Recipient, IO.Swagger.Models.Recipient>();
+                cfg.CreateMap<Entities.Warehouse, IO.Swagger.Models.Warehouse>();
+                cfg.CreateMap<Entities.Truck, IO.Swagger.Models.Truck>();
+                cfg.CreateMap<Entities.TrackingInformation, IO.Swagger.Models.TrackingInformation>();
+                cfg.CreateMap<Entities.HopArrival, IO.Swagger.Models.HopArrival>();
+                //DAL --> BL
                 cfg.CreateMap<DataAccess.Entities.Recipient, Entities.Recipient>();
                 cfg.CreateMap<DataAccess.Entities.Parcel, Entities.Parcel>();
                 cfg.CreateMap<DataAccess.Entities.Warehouse, Entities.Warehouse>();
                 cfg.CreateMap<DataAccess.Entities.Truck, Entities.Truck>();
                 cfg.CreateMap<DataAccess.Entities.TrackingInformation, Entities.TrackingInformation>();
                 cfg.CreateMap<DataAccess.Entities.HopArrival, Entities.HopArrival>();
-
-                cfg.CreateMap<Entities.Parcel, IO.Swagger.Models.Parcel>();
-                cfg.CreateMap<Entities.Recipient, IO.Swagger.Models.Recipient>();
-
+                //BL --> DAL
+                cfg.CreateMap<Entities.Recipient, DataAccess.Entities.Recipient>()
+                    .ForMember(model => model.id, option => option.Ignore());
+                cfg.CreateMap<Entities.Parcel, DataAccess.Entities.Parcel>()
+                    .ForMember(model => model.Id, option => option.Ignore())
+                    .ForMember(model => model.TrackingInformationId, option => option.Ignore())
+                    .ForMember(model => model.RecipientId, option => option.Ignore());
+                cfg.CreateMap<Entities.Warehouse, DataAccess.Entities.Warehouse>()
+                    .ForMember(model => model.id, option => option.Ignore());
+                cfg.CreateMap<Entities.Truck, DataAccess.Entities.Truck>()
+                    .ForMember(model => model.id, option => option.Ignore());
+                cfg.CreateMap<Entities.TrackingInformation, DataAccess.Entities.TrackingInformation>()
+                    .ForMember(model => model.id, option => option.Ignore());
+                cfg.CreateMap<Entities.HopArrival, DataAccess.Entities.HopArrival>()
+                    .ForMember(model => model.id, option => option.Ignore())
+                    .ForMember(model => model.trackingInformationId, option => option.Ignore());
             }
             );
 
