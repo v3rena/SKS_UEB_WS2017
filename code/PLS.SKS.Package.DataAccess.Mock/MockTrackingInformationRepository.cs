0cff73e9cf9da03789ff28fe1f660279c1b25832
MrQorm <tobias.nemecek@gmx.at>
2017-11-26 21:10:37 +0100

    AddParcel Test eingefügt

--- code/PLS.SKS.Package.DataAccess.Mock/MockTrackingInformationRepository.cs
+++ code/PLS.SKS.Package.DataAccess.Mock/MockTrackingInformationRepository.cs
@@ -10,12 +10,22 @@ namespace PLS.SKS.Package.DataAccess.Mock
 	public class MockTrackingInformationRepository : ITrackingInformationRepository
 	{
 		public List<TrackingInformation> trackingInformations = new List<TrackingInformation>();
-		int t_id = 0;
+		int t_id = 2;
+
+        public MockTrackingInformationRepository()
+        {
+            var trackInfos = new TrackingInformation[]
+            {
+                new TrackingInformation{Id = 1, State=TrackingInformation.StateEnum.DeliveredEnum },
+                new TrackingInformation{Id = 2, State=TrackingInformation.StateEnum.InTransportEnum }
+            };
+            trackingInformations = trackInfos.ToList();
+        }
 
 		public int Create(TrackingInformation t)
 		{
-			t.Id = t_id;
-			t_id++;
+            t_id++;
+            t.Id = t_id;
 			trackingInformations.Add(t);
 			return t.Id;
 		}
